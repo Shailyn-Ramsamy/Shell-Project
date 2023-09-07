5b2e550aa0bbaebdae65749d5c8a60bf326aebe8
Shailyn-Ramsamy <shailyn.ramsamy@gmail.com>
2023-09-07 20:34:11 +0200

    hello

--- witsshell.c
+++ witsshell.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <assert.h>
 #include <fcntl.h>
+#include <ctype.h>
 char *search_paths[11];
 
 #define MAX_INPUT_SIZE 1024
@@ -17,12 +18,14 @@ void built_in(char *all_args[], int num_args){
         exit(0);
     }
     else if (strcmp(all_args[0], "exit") == 0 && num_args > 1){
-        fprintf(stderr, "An error has occurred\n");
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
         exit(0);
     }
     else if (strcmp(all_args[0],"cd")==0){
         if(num_args != 2){
-            fprintf(stderr, "An error has occurred\n");
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
         }
         chdir(all_args[1]);
     }
@@ -45,11 +48,12 @@ void execute_command(char *input) {
     char *all_args[11];
     int num_args = 0;
     bool redirect = false;
+    bool executable = false;
     char *output_filename = NULL;
 
     input_dupe = strdup(input);
 
-    while ((arguments = strsep(&input_dupe, " ")) != NULL && num_args < 10) {
+    while ((arguments = strsep(&input_dupe, " ")) != NULL && num_args < 10 ) {
         if (strcmp(arguments, ">") == 0) {
             redirect = true;
             if ((arguments = strsep(&input_dupe, " ")) != NULL) {
@@ -57,66 +61,78 @@ void execute_command(char *input) {
             }
 
             if ((arguments = strsep(&input_dupe, " ")) != NULL) {
-                fprintf(stderr, "Error: Multiple arguments after `>`.\n");
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
                 exit(0);
             }
 
-            break; 
+            break;
         }
-        all_args[num_args++] = arguments;
+
+        if (arguments[0] != '\0') { // Check if the string is not empty
+            all_args[num_args++] = arguments;
+        }
     }
 
 
-    if (num_args >= 2 && strcmp(all_args[num_args - 2], ">") == 0) {
-        redirect = true;
-    }
-
-    if (strcmp(all_args[0], "cd") == 0 || strcmp(all_args[0], "exit") == 0 || strcmp(all_args[0],"path") == 0) {
+    if (strcmp(all_args[0], "cd") == 0 || strcmp(all_args[0], "exit") == 0 || strcmp(all_args[0], "path") == 0) {
         built_in(all_args, num_args);
-    }
 
-
-    else {
+        if (strcmp(all_args[0], "path") == 0) {
+            executable = false;
+        }
+    } else {
         int num_paths = 0;
-        while(search_paths[num_paths] != NULL){
+        while (search_paths[num_paths] != NULL) {
             all_args[num_args] = NULL;
             char executable_path[MAX_INPUT_SIZE];
 
             strcpy(executable_path, search_paths[num_paths]);
-//            if (search_paths[num_paths][strlen(search_paths[num_paths]) - 1] != '/') {
-//                strcat(executable_path, "/");
-//            }
+            if (search_paths[num_paths][strlen(search_paths[num_paths]) - 1] != '/') {
+                strcat(executable_path, "/");
+            }
             strcat(executable_path, all_args[0]);
-            printf("Full path for %s: %s\n", all_args[0], executable_path);
 
-
-            pid_t pid = fork();
-            if (pid == -1) {
-                perror("fork");
-                exit(1);
-            } else if (pid == 0) {
-                if (redirect) {
-                    // Redirect output to a file
-                    int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-                    if (fd == -1) {
-                        perror("open");
+            // Check if the file at executable_path is executable
+            if (access(executable_path, X_OK) == 0) {
+                pid_t pid = fork();
+                if (pid == -1) {
+                    char error_message[30] = "An error has occurred\n";
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                    exit(1);
+                } else if (pid == 0) {
+                    if (redirect) {
+                        int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                        if (fd == -1) {
+                            char error_message[30] = "An error has occurred\n";
+                            write(STDERR_FILENO, error_message, strlen(error_message));
+                            exit(1);
+                        }
+                        dup2(fd, STDOUT_FILENO);
+                        close(fd);
+                    }
+                    if (execvp(executable_path, all_args) == -1) {
                         exit(1);
                     }
-                    dup2(fd, STDOUT_FILENO);
-                    close(fd);
+                    // No need to break here, the child process will exit
+                } else {
+                    int status;
+                    waitpid(pid, &status, 0);
+                    executable = true;
+                    break;
                 }
-                if (execvp(executable_path, all_args) == 0) {
-                    fprintf(stderr, "An error has occurred\n");
-                    exit(1);
-                }
-            } else {
-                int status;
-                waitpid(pid, &status, 0);
             }
+            else{
+                executable = false;
+            }
+
             num_paths++;
         }
+        if (!executable){
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
+        }
     }
-
 
     free(input_dupe);
 }
@@ -138,7 +154,8 @@ void interactive_mode(){
                 printf("\n");
                 exit(0);
             } else {
-                perror("getline");
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
                 free(input);
                 exit(1);
             }
@@ -147,14 +164,29 @@ void interactive_mode(){
             input[length - 1] = '\0';
         }
 
+        int isEmpty = 1;
+        for (int i = 0; input[i] != '\0'; i++) {
+            if (!isspace((unsigned char)input[i])) {
+                isEmpty = 0;
+                break;
+            }
+        }
+
+        if (isEmpty) {
+            free(input);
+            continue;
+        }
+
         execute_command(input);
+        free(input);
     }
 }
 
 void batch_mode(const char *filename){
     FILE *batch = fopen(filename, "r");
     if (batch == NULL) {
-        perror("Error opening batch file");
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
 
@@ -163,6 +195,18 @@ void batch_mode(const char *filename){
 
     while (getline(&input, &bufsize, batch) != -1){
         input[strcspn(input, "\n")] = '\0';
+        int isEmpty = 1;
+        for (int i = 0; input[i] != '\0'; i++) {
+            if (!isspace((unsigned char)input[i])) {
+                isEmpty = 0;
+                break;
+            }
+        }
+
+        if (isEmpty) {
+            free(input);
+            continue;
+        }
         execute_command(input);
     }
 
@@ -183,7 +227,8 @@ int main(int MainArgc, char *MainArgv[]){
         batch_mode(MainArgv[1]);
     }
     else {
-        printf("Usage: %s [batch_file]\n", MainArgv[0]);
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
 
