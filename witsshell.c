7bbbf0b22fb750e94cd19a62e9ccc58958d13504
Shailyn-Ramsamy <shailyn.ramsamy@gmail.com>
2023-09-11 20:08:21 +0200

    completed

--- witsshell.c
+++ witsshell.c
@@ -7,12 +7,11 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <ctype.h>
-char *search_paths[11];
+char *search_paths[32];
 
 #define MAX_INPUT_SIZE 1024
 
-char *trim_whitespace(char *str) {
-    // Skip leading whitespace characters
+char *trim(char *str) {
     while (isspace((unsigned char)*str)) {
         str++;
     }
@@ -40,7 +39,7 @@ void built_in(char *all_args[], int num_args){
     }
     else if (strcmp(all_args[0],"path")==0){
 
-        for (int i =0; i < 11; i++){
+        for (int i =0; i < 32; i++){
             search_paths[i] = NULL;
         }
 
@@ -56,8 +55,8 @@ void execute_command(char *input) {
     char *input_dupe = NULL;
     char *arguments;
     char *argumentstest;
-    char *all_args[11];
-    char *all_argstest[11];
+    char *all_args[32];
+    char *all_argstest[32];
     int num_args = 0;
     int num_argstests = 0;
     bool redirect = false;
@@ -80,11 +79,12 @@ void execute_command(char *input) {
         exit(0);
     }
 
-    while ((argumentstest= strsep(&input_dupe, ">")) != NULL && num_argstests <10){
+    while ((argumentstest= strsep(&input_dupe, ">")) != NULL && num_argstests <32){
         if (argumentstest[0] != '\0') {
             all_argstest[num_argstests++] = argumentstest;
         }
     }
+
 
     if (out_count == 1 && num_argstests == 1){
         char error_message[30] = "An error has occurred\n";
@@ -102,16 +102,14 @@ void execute_command(char *input) {
         output_filename = all_argstest[1];
     }
 
-    while ((arguments = strsep(&all_argstest[0], " ")) != NULL && num_args < 10 ) {
+
+
+    while ((arguments = strsep(&all_argstest[0], " ")) != NULL && num_args < 32 ) {
 
         if (arguments[0] != '\0') {
             all_args[num_args++] = arguments;
         }
     }
-
-
-//    if (strcmp(all_args[0], "cd") == 0 || strcmp(all_args[0], "exit") == 0) {
-//        built_in(all_args, num_args);
 
 
     int num_paths = 0;
@@ -133,7 +131,12 @@ void execute_command(char *input) {
                 exit(1);
             } else if (pid == 0) {
                 if (redirect) {
-                    output_filename = trim_whitespace(output_filename);
+                    output_filename = trim(output_filename);
+                    if (strchr(output_filename, ' ') != NULL) {
+                        char error_message[30] = "An error has occurred\n";
+                        write(STDERR_FILENO, error_message, strlen(error_message));
+                        exit(1);
+                    }
                     int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
                     if (fd == -1) {
                         char error_message[30] = "An error has occurred\n";
@@ -171,24 +174,46 @@ void execute_command(char *input) {
 void execute_parallel_command(char *commands){
     char *command_dupe = NULL;
     char *command;
-    char *command_arr[11];
+    char *command_arr[32];
     int num_commands = 0;
     char *temp;
     int num_temp = 0;
-    char *temp_arr[11];
-    char *command_arr_duplicate[11];
+    char *temp_arr[32];
+    char *command_arr_duplicate[32];
     int num_commands_duplicate = 0;
 
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < 32; i++){
         command_arr[i] = NULL;
+    }
+
+    for (int i = 0; i < 32; i++){
+        command_arr_duplicate[i] = NULL;
+    }
+
+    for (int i = 0; i < 32; i++){
+        temp_arr[i] = NULL;
     }
 
     command_dupe = strdup(commands);
 
-    while ((command = strsep(&command_dupe, "&")) != NULL && num_commands < 10) {
+    while ((command = strsep(&command_dupe, "&")) != NULL && num_commands < 32) {
         if (command[0] != '\0') {
-            command_arr[num_commands++] = trim_whitespace(command);
+            command_arr[num_commands++] = trim(command);
         }
+    }
+
+    bool allNull = true;
+    for (int i = 0; i < num_commands; i++) {
+        if (command_arr[i] != NULL) {
+            allNull = false;
+            break;
+        }
+    }
+
+    if (allNull) {
+        command_arr[0] = " ";
+        command_arr[1] = " ";
+        num_commands = 2;
     }
 
     for (int i = 0; i < num_commands; i++) {
@@ -203,14 +228,15 @@ void execute_parallel_command(char *commands){
     }
 
 
-    while ((temp = strsep(&command_arr_duplicate[0], " ")) != NULL && num_temp < 10){
-        temp_arr[num_temp++] = trim_whitespace(temp);
+    while ((temp = strsep(&command_arr_duplicate[0], " ")) != NULL && num_temp < 32){
+        temp_arr[num_temp++] = trim(temp);
     }
 
     if (strcmp(temp_arr[0], "cd") == 0 || strcmp(temp_arr[0], "exit") == 0 || strcmp(temp_arr[0], "path") == 0) {
         built_in(temp_arr, num_temp);
     }
     else{
+
         for (int i = 0; i < num_commands; i++) {
             char *current_command = command_arr[i];
             int pid = fork();
@@ -230,10 +256,6 @@ void execute_parallel_command(char *commands){
             wait(NULL);
         }
     }
-
-
-    // Wait for all child processes to finish
-
 
     free(command_dupe);
 }
@@ -283,7 +305,7 @@ void interactive_mode(){
     }
 }
 
-void batch_mode(const char *filename){
+void batch_mode(const char *filename) {
     FILE *batch = fopen(filename, "r");
     if (batch == NULL) {
         char error_message[30] = "An error has occurred\n";
@@ -294,20 +316,37 @@ void batch_mode(const char *filename){
     char *input = NULL;
     size_t bufsize = 0;
 
-    while (getline(&input, &bufsize, batch) != -1){
-        input[strcspn(input, "\n")] = '\0';
+    while (1) {
+        ssize_t length = getline(&input, &bufsize, batch);
+
+        if (length == -1) {
+            if (feof(batch)) {
+                free(input);
+                break;
+            } else {
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                free(input);
+                exit(1);
+            }
+        }
+        if (length > 0 && input[length - 1] == '\n') {
+            input[length - 1] = '\0';
+            length--;
+        }
+
         int isEmpty = 1;
-        for (int i = 0; input[i] != '\0'; i++) {
-            if (!isspace((unsigned char)input[i])) {
+        for (ssize_t i = 0; i < length; i++) {
+            if (!isspace((unsigned char) input[i])) {
                 isEmpty = 0;
                 break;
             }
         }
 
         if (isEmpty) {
-            free(input);
             continue;
         }
+
         execute_parallel_command(input);
     }
 
@@ -315,8 +354,8 @@ void batch_mode(const char *filename){
 }
 
 int main(int MainArgc, char *MainArgv[]){
-    for (int i =0; i < 11; i++){
-        free(search_paths[i]);
+    for (int i =0; i < 32; i++){
+        search_paths[i] = NULL;
     }
 
     search_paths[0]= "/bin/";
@@ -332,6 +371,8 @@ int main(int MainArgc, char *MainArgv[]){
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
+
+
 
     return(0);
 }
